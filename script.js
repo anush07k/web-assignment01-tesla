742a3cf2ddb3049da952c3433799aa60b47172a3
Anushka Dubey <120951444+anush07k@users.noreply.github.com>
2023-05-22 22:42:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,11 @@ function calculateNotes() {
     
     var notes = [2000, 500, 200, 100, 50, 20, 10, 5, 1];
     var noteCounts = {};
-
-    // Initialize note counts
     for (var i = 0; i < notes.length; i++) {
         var note = notes[i];
         noteCounts[note] = 0;
     }
 
-    // Calculate note counts
     for (var i = 0; i < notes.length; i++) {
         var note = notes[i];
         if (amount >= note) {
@@ -19,13 +16,11 @@ function calculateNotes() {
         }
     }
 
-    // Update the note count display
     for (var i = 0; i < notes.length; i++) {
         var note = notes[i];
         document.getElementById('note' + note).textContent = noteCounts[note];
     }
 
-    // Calculate total notes
     var totalNotes = Object.values(noteCounts).reduce(function(a, b) {
         return a + b;
     }, 0);
